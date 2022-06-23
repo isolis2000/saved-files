740f622a9425fa3592570471bc96eb4d37a8b3e6
Iván Solís Ávila <isolis@solavcr.com>
2022-06-23 16:12:44 -0600

    Add files via upload

---
+++ flatInstallScript.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Script for installing packages through flathub
+sudo flatpak install -y com.axosoft.GitKraken
+sudo flatpak install -y com.bitwarden.desktop
+sudo flatpak install -y com.discordapp.Discord
+sudo flatpak install -y com.getpostman.Postman
+sudo flatpak install -y com.github.gi_lom.dialect
+sudo flatpak install -y com.microsoft.Teams 
+sudo flatpak install -y com.rafaelmardojai.Blanket
+sudo flatpak install -y com.spotify.Client
+sudo flatpak install -y com.visualstudio.code
+sudo flatpak install -y org.apache.netbeans
+sudo flatpak install -y org.deluge_torrent.deluge
+sudo flatpak install -y org.kryogenix.Pick
+sudo flatpak install -y org.onlyoffice.desktopeditors
+sudo flatpak install -y org.videolan.VLC
+sudo flatpak install -y us.zoom.Zoom
+sudo flatpak install -y flathub org.mozilla.Thunderbird
+sudo flatpak install -y org.telegram.desktop
