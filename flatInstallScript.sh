cd6347e58441132f63c66b22525b98cd31892c2c
Iván Solís Ávila <isolis@solavcr.com>
2022-07-09 03:10:33 -0600

    Update flatInstallScript.sh

--- flatInstallScript.sh
+++ flatInstallScript.sh
@@ -16,3 +16,25 @@ sudo flatpak install -y org.onlyoffice.desktopeditors
 sudo flatpak install -y org.videolan.VLC
 sudo flatpak install -y us.zoom.Zoom
 sudo flatpak install -y org.telegram.desktop
+
+# When using Fedora
+
+# dnf
+sudo dnf install -y discord
+sudo dnf install -y dialect
+sudo dnf install -y deluge
+sudo dnf install -y gpick
+sudo dnf install -y vlc
+sudo dnf install -y telegram
+
+# flatpak
+sudo flatpak install -y com.rafaelmardojai.Blanket
+sudo flatpak install -y com.microsoft.Teams
+sudo flatpak install -y com.axosoft.GitKraken
+sudo flatpak install -y com.bitwarden.desktop
+sudo flatpak install -y com.getpostman.Postman
+sudo flatpak install -y com.spotify.Client
+sudo flatpak install -y com.visualstudio.code
+sudo flatpak install -y org.apache.netbeans
+sudo flatpak install -y org.onlyoffice.desktopeditors
+sudo flatpak install -y us.zoom.Zoom
