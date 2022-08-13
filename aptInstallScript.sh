f8878c06d9f591677407ecfa88c5046657a6cbf8
Iván Solís Ávila <isolis@solavcr.com>
2022-08-13 02:59:30 +0000

    Create aptInstallScript.sh

---
+++ aptInstallScript.sh
@@ -0,0 +1,24 @@
+# Linux Mint
+
+# apt
+sudo apt install -y micro
+sudo apt install -y gpick
+sudo apt install -y dialect
+sudo apt install -y deluge
+sudo apt install -y vlc
+sudo apt install -y tilix
+sudo apt install -y build-essential
+
+# flatpak
+sudo flatpak install -y com.bitwarden.desktop
+sudo flatpak install -y ab.librewolf-community
+sudo flatpak install -y rafaelmardojai.Blanket
+sudo flatpak install -y com.microsoft.Teams
+sudo flatpak install -y com.getpostman.Postman
+sudo flatpak install -y com.spotify.Client
+sudo flatpak install -y com.visualstudio.code
+sudo flatpak install -y org.onlyoffice.desktopeditors
+sudo flatpak install -y us.zoom.Zoom
+sudo flatpak install -y org.telegram.desktop
+sudo flatpak install -y org.apache.netbeans
+sudo flatpak install -y com.discordapp.Discord
