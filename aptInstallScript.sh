fbe1e9592984d514732a779bbf31f3a0d74da296
Iván Solís Ávila <isolis@solavcr.com>
2023-07-10 17:59:33 -0600

    Update aptInstallScript.sh

--- aptInstallScript.sh
+++ aptInstallScript.sh
@@ -1,16 +1,17 @@
 # Linux Mint
+allup
 
 # apt
 sudo apt install -y gpick
 sudo apt install -y deluge
 sudo apt install -y vlc
+sudo apt install -y gnome-boxes
 sudo apt install -y build-essential
 
 # flatpak
 flatpak install flathub -y com.discordapp.Discord
 flatpak install flathub -y com.bitwarden.desktop
 flatpak install flathub -y rafaelmardojai.Blanket
-flatpak install flathub -y com.microsoft.Teams
 flatpak install flathub -y com.getpostman.Postman
 flatpak install flathub -y com.spotify.Client
 flatpak install flathub -y org.onlyoffice.desktopeditors
