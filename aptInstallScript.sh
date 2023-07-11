2f7ecc0dcd4260fe5e7835099439bc972dc79c40
isolis2000 <isolis@solavcr.com>
2023-07-10 18:43:09 -0600

    updated aptinstallscript.sh

--- aptInstallScript.sh
+++ aptInstallScript.sh
@@ -1,11 +1,25 @@
 # Linux Mint
-allup
+
+# microsoft stuff
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo add-apt-repository multiverse
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && flatpak update -y && sudo snap refresh
+sudo apt install code # or code-insiders
+sudo apt install ttf-mscorefonts-installer
+sudo fc-cache -f -v
 
 # apt
 sudo apt install -y gpick
 sudo apt install -y deluge
 sudo apt install -y vlc
 sudo apt install -y gnome-boxes
+sudo apt install -y neofetch
+sudo apt install -y bpytop
 sudo apt install -y build-essential
 
 # flatpak
@@ -19,12 +33,3 @@ flatpak install flathub -y us.zoom.Zoom
 flatpak install flathub -y org.telegram.desktop
 flatpak install flathub -y org.apache.netbeans
 
-# vscode
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code # or code-insiders
