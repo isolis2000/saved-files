16977fe8ef6bd1fbf8dcf67822ea972439071ce2
isolis2000 <isolis@solavcr.com>
2023-07-10 18:16:57 -0600

    updated aptInstallScript.sh with vscode instructions

--- aptInstallScript.sh
+++ aptInstallScript.sh
@@ -18,3 +18,13 @@ flatpak install flathub -y org.onlyoffice.desktopeditors
 flatpak install flathub -y us.zoom.Zoom
 flatpak install flathub -y org.telegram.desktop
 flatpak install flathub -y org.apache.netbeans
+
+# vscode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
